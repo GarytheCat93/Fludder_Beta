ab57eadb1e3ce3b7fc17386dd2406ce1be446a10
Gary Gretzky <jdog112325@yahoo.com>
2020-01-25 03:16:08 -0500

    1ST Foundation Push!!! Nothing SHOULD EVER EVER be REVERTED before this push. This is the foundataion of the beta running successfully on iOS and Android

---
+++ Fludder_Beta_VS/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Fludder_Beta_VS.Services;
+using Fludder_Beta_VS.Views;
+
+namespace Fludder_Beta_VS
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Fludder_Beta_VS/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Fludder_Beta_VS.Models;
+
+namespace Fludder_Beta_VS.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
